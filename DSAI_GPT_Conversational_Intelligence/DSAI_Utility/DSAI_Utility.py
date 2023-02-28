e6b85a5980d9bd8a42838859b0d905ec4f9b0be3
ds_006 <DeepsphereAIInfo@gmail.com>
2023-02-28 15:11:58 +0530

    conversational intelligence initial commit

---
+++ DSAI_GPT_Conversational_Intelligence/DSAI_Utility/DSAI_Utility.py
@@ -0,0 +1,36 @@
+
+import streamlit as st
+from PIL import Image
+
+
+def CSS_Property(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def All_Initialization():
+    image = Image.open('DSAI_Utility/Logo_final.png')
+    st.image(image)
+    st.markdown("<h1 style='text-align: center; color: #454545; font-size:25px;'>ChatGPT Application for Human Resource</h1><h2 style='text-align: center; color: blue; font-size:20px;'>ChatGPT Classifies Live Conversation by Hate, Insult, Threat, Obscene, and Profanity</h2>", unsafe_allow_html=True)
+    st.markdown("""
+    <hr style="width:100%;height:3px;background-color:gray;border-width:10">
+    """, unsafe_allow_html=True)
+    choice1 =  st.sidebar.selectbox(" ",('Home','About Us'))
+    choice2 =  st.sidebar.selectbox(" ",('Libraries in Scope','OpenAI','pyChatGPT','Pandas','Streamlit'))
+    choice3 =  st.sidebar.selectbox(" ",('Models Used','ChatGPT', 'GPT3', 'GPT3 - Ada','GPT3 - Babbage','GPT3 - Davinci'))
+    menu = ["Google Cloud Services in Scope","Cloud Storage", "Cloud Run", "Cloud Function", "Secret Manager"]
+    choice = st.sidebar.selectbox(" ",menu)
+    st.sidebar.write('')
+    st.sidebar.write('')
+    href = """<form action="#">
+    <input type="submit" value="Clear/Reset" />
+</form>"""
+    st.sidebar.markdown(href, unsafe_allow_html=True)
+    st.sidebar.write('')
+    st.sidebar.write('')
+    st.sidebar.text('Build & Deployed on')
+    st.sidebar.write('')
+    st.sidebar.image('DSAI_Utility/Google-Cloud-Platform-GCP-logo.png')
+    
+    # vAR_clear_button = st.sidebar.button('Clear/Reset')
+    # if vAR_clear_button:
+    #     st.experimental_rerun()
